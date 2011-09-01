45b9f598e0a05716d4b13f17073c8367424365d2
russellballestrini <devnull@localhost>
2011-08-31 22:10:47 -0400

    migrated routes to pyramid 1.1 syntax

--- pyrapad/__init__.py
+++ pyrapad/__init__.py
@@ -17,14 +17,30 @@ def main(global_config, **settings):
     config.add_subscriber( inject_renderer_globals, BeforeRender )
 
     config.add_static_view('static', 'pyrapad:static')
-    config.add_route( 'home', '/', view='pyrapad.views.add', view_renderer='add.mako' )
-    config.add_route( 'add', '/add', view='pyrapad.views.add', view_renderer='add.mako' )
-    config.add_route( 'random', '/random', view='pyrapad.views.random' )
-    config.add_route( 'recent', '/recent', view='pyrapad.views.recent', view_renderer='recent.mako' )
-    config.add_route( 'syntaxes', '/syntaxes', view='pyrapad.views.syntaxes', view_renderer='syntaxes.mako' )
-    config.add_route( 'reply', '/{id}/reply', view='pyrapad.views.reply' )
-    config.add_route( 'show', '{id}/{uri:.*}', view='pyrapad.views.show', view_renderer='show.mako' )
-    config.add_route( 'show2', '{id}', view='pyrapad.views.show', view_renderer='show.mako' )
+
+    config.add_route( 'home', '/' )
+    config.add_view( 'pyrapad.views.add', route_name='home', renderer='add.mako' )
+
+    config.add_route( 'add', '/add' )
+    config.add_view( 'pyrapad.views.add', route_name='add', renderer='add.mako' )
+
+    config.add_route( 'random', '/random' )
+    config.add_view( 'pyrapad.views.random', route_name='random' )
+
+    config.add_route( 'recent', '/recent' )
+    config.add_view( 'pyrapad.views.recent', route_name='recent', renderer='recent.mako' )
+
+    config.add_route( 'syntaxes', '/syntaxes' )
+    config.add_view( 'pyrapad.views.syntaxes', route_name='syntaxes', renderer='syntaxes.mako' )
+
+    config.add_route( 'reply', '/{id}/reply' )
+    config.add_view( 'pyrapad.views.reply', route_name='reply' )
+
+    config.add_route( 'show', '{id}/{uri:.*}' )
+    config.add_view( 'pyrapad.views.show', route_name='show', renderer='show.mako' )
+
+    config.add_route( 'show2', '{id}' )
+    config.add_view( 'pyrapad.views.show', route_name='show2', renderer='show.mako' )
 
     return config.make_wsgi_app()
 
