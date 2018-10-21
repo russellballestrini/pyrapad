96a06e1d0d9a4a85cb92cfff30768509a21f1a22
russellballestrini <devnull@localhost>
2018-10-21 09:35:23 -0400

    mess with stuff
    changed .hgignore
    changed README.txt
    changed setup.py
    removed CHANGES.txt

--- README.txt
+++ README.txt
@@ -1,4 +1,13 @@
-pyrapad README
+pyrapad
+########
 
+This repo powers https://pad.yohdah.com
 
+fedora dev env
+==============
 
+You will need the mysql dev files
+
+.. code-block: bash
+
+  dnf install mariadb-devel
--- setup.py
+++ setup.py
@@ -4,53 +4,55 @@ import sys
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+README = open(os.path.join(here, "README.txt")).read()
 
 requires = [
-    'pyramid',
-    'pyramid_mako',
-    'pyramid_debugtoolbar',
-    'pyramid_tm',
-    'SQLAlchemy',
-    'transaction',
-    'MySQL-python',
-    'zope.sqlalchemy',
-    'pygments', # syntax highlighting
-    'webhelpers', # pagination
-    'ago', # human readable timedelta
-    'pastescript', # paster
-    'slugify', # powerful uri slug lib
-    'waitress', # web application server for development
-    'uwsgi', # web application server for production
+    # Pyramid.
+    "pyramid",
+    "pyramid_mako",
+    "pyramid_debugtoolbar",
+    "pyramid_tm",
+    "pyramid_retry",
+    "plaster_pastedeploy",
+
+    # Model.
+    #"MySQL-python",
+    "mysqlclient",
+    "SQLAlchemy",
+    "transaction",
+    "zope.sqlalchemy",
+
+    # misc.
+    #"pastescript", # paster
+    "pygments", # syntax highlighting
+    "webhelpers", # pagination
+    "ago", # human readable timedelta
+    "slugify", # powerful uri slug lib
+    "waitress", # web application server for development
+    "uwsgi", # web application server for production
     ]
 
-if sys.version_info[:3] < (2,5,0):
-    requires.append('pysqlite')
-
-setup(name='pyrapad',
-      version='0.0',
-      description='pyrapad',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pylons",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='pyrapad',
-      install_requires = requires,
-      entry_points = """\
-      [paste.app_factory]
-      main = pyrapad:main
-      """,
-      paster_plugins=['pyramid'],
-      )
-
+setup(
+    name="pyrapad",
+    version="0.1.0",
+    description="pyrapad",
+    long_description=README,
+    classifiers=[
+      "Programming Language :: Python",
+      "Framework :: Pyramid"
+      "Topic :: Internet :: WWW/HTTP",
+      "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+    author="Russell Ballestrini",
+    author_email="russell@ballestrini.net",
+    url="https://pad.yohdah.com",
+    keywords="pyramid application paste code pad app",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="pyrapad",
+    install_requires = requires,
+    entry_points = {
+        "paste.app_factory": ["main = pyrapad:main"],
+    },
+)
